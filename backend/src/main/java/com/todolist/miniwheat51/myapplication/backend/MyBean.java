3fc802a9c7473656db6ffbee6fc5c9f7c035a161
sp08c <sp08c@my.fsu.edu>
2016-07-20 17:51:45 -0400

    Add files via upload
    I hope I did this right.  Here are what I think are the files that pertain to adding a backend to the app.

---
+++ backend/src/main/java/com/todolist/miniwheat51/myapplication/backend/MyBean.java
@@ -0,0 +1,15 @@
+package com.todolist.miniwheat51.myapplication.backend;
+
+/** The object model for the data we are sending through endpoints */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
